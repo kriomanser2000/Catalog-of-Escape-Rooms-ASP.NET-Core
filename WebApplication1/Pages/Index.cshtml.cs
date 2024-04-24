545e6830984223666ead6dc96a0bb0ec4eb03d25
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-04-24 12:39:18 +0300

    added

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -1,5 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+using System.Data.SqlClient;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Net;
+using System.Xml.Linq;
 
 namespace WebApplication1.Pages
 {
