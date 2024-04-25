e5ed674193753b2a616bea7186e57aeb492fd4fa
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-04-25 15:01:42 +0300

    added footer for all html(But not customized)

--- WebApplication1/Pages/ROOMS11/RoomsInfo.cs
+++ WebApplication1/Pages/ROOMS11/RoomsInfo.cs
@@ -5,46 +5,25 @@ namespace WebApplication1.Pages.ROOMS11
     public class RoomsInfo
     {
         public int Id { get; set; }
-
-        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
-
         public string About { get; set; }
-
-        [Range(30, 60, ErrorMessage = "Min 30 Max 60")]
+        [Range(30, 60)]
         public int TimeLimit { get; set; }
-
-        [Range(1, int.MaxValue, ErrorMessage = "GroupSizeMin must be greater than 0")]
+        [Range(1, int.MaxValue)]
         public int GroupSizeMin { get; set; }
-
-        [Range(1, int.MaxValue, ErrorMessage = "GroupSizeMax must be greater than 0")]
+        [Range(1, int.MaxValue)]
         public int GroupSizeMax { get; set; }
-
-        [Range(1, 100, ErrorMessage = "MinAge must be between 1 and 100")]
+        [Range(1, 100)]
         public int MinAge { get; set; }
-
-        [Required(ErrorMessage = "Address is required")]
         public string Address { get; set; }
-
-        [Required(ErrorMessage = "PhoneNum is required")]
         public int PhoneNum { get; set; }
-
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
-
-        [Required(ErrorMessage = "CompanyName is required")]
         public string CompanyName { get; set; }
-
-        [Required(ErrorMessage = "Rate is required")]
         public string Rate { get; set; }
-
-        [Range(1, 5, ErrorMessage = "FearLvl must be between 1 and 5")]
+        [Range(1, 5)]
         public int FearLvl { get; set; }
-
-        [Range(1, 5, ErrorMessage = "Difficult must be between 1 and 5")]
+        [Range(1, 5)]
         public int Difficult { get; set; }
-
         [DataType(DataType.Upload)]
         public byte[] GalleryImg { get; set; }
     }
