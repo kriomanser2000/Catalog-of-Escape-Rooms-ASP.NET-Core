624c0957e70b97b688a4718cc95d0db3a3de9c77
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-04-26 07:18:10 +0300

    deleted OLD bd Table, fixed Footer

--- WebApplication1/Pages/ROOMS11/QuestList.cshtml.cs
+++ WebApplication1/Pages/ROOMS11/QuestList.cshtml.cs
@@ -11,53 +11,9 @@ namespace WebApplication1.Pages.ROOMS11
 {
     public class QuestListModel : PageModel
     {
-        private readonly ILogger<QuestListModel> _logger;
-        public QuestListModel(ILogger<QuestListModel> logger)
-        {
-            _logger = logger;
-        }
-        public List<RoomsInfo> listRooms = new List<RoomsInfo>();
         public void OnGet()
         {
-            try
-            {
-                string connectionString = "Data Source=DESKTOP-MQCJ73U\\SQLEXPRESS;Initial Catalog=CatalogOfRooms;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    string sql = "SELECT * FROM rooms";
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                RoomsInfo rooms = new RoomsInfo();
-                                rooms.Id = reader.GetInt32(0);
-                                rooms.Name = reader.GetString(1);
-                                rooms.About = reader.GetString(2);
-                                rooms.TimeLimit = reader.GetInt32(3);
-                                rooms.GroupSizeMin = reader.GetInt32(4);
-                                rooms.GroupSizeMax = reader.GetInt32(5);
-                                rooms.MinAge = reader.GetInt32(6);
-                                rooms.Address = reader.GetString(7);
-                                rooms.PhoneNum = reader.GetInt32(8);
-                                rooms.Email = reader.GetString(9);
-                                rooms.CompanyName = reader.GetString(10);
-                                rooms.Rate = reader.GetString(11);
-                                rooms.FearLvl = reader.GetInt32(12);
-                                rooms.Difficult = reader.GetInt32(13);
-                                rooms.GalleryImg = (byte[])reader["GalleryImg"];
-                                listRooms.Add(rooms);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-
-            }
+            
         }
     }
 }
