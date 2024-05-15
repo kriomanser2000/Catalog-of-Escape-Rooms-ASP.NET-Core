b95e8424742350eabb29ef76f1de0d5e159dd749
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-05-15 16:52:56 +0300

    database fixed

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-//DI wht
+//di wht
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 //builder.Services.AddDbContext<RoomContext>(options =>
